599611926dacc3824d54601c5b2eafd07f34ad28
brntmedia <brntmedia@gmail.com>
2020-09-26 21:50:20 -0400

    adding testimonial html

--- home-page.js
+++ home-page.js
@@ -39,6 +39,13 @@ closeFunMenu.onclick=function(){
     funMenu.style.width='0px';
 }
 
+/*let vh = window.innerHeight*0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`)
+
+window.addEventListener('resize',() => {
+    let vh = window.innerHeight*0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`)
+})*/
 
 /*var form=document.getElementById("modalForm");
 var button=document.getElementById("button");
